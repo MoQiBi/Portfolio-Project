a057b8b46b2311c2412a1baa24ec9796608e921b
Afif Naqib <65846216+MoQiBi@users.noreply.github.com>
2025-02-21 13:13:55 +0800

    Update Covid-19 Exploratory Data Analysis as at August 2024.sql

--- Covid-19 Exploratory Data Analysis as at August 2024.sql
+++ Covid-19 Exploratory Data Analysis as at August 2024.sql
@@ -1,3 +1,114 @@
+#IMPORTING DATA (PowerShell)
+DROP TABLE IF EXISTS covid_vaccinations;
+CREATE TABLE covid_vaccinations (
+iso_code TEXT,
+continent TEXT,
+location TEXT,
+`date` TEXT,
+new_tests INT,
+total_tests_per_thousand DECIMAL,
+new_tests_per_thousand DECIMAL,
+new_tests_smoothed DECIMAL,
+new_tests_smoothed_per_thousand DECIMAL,
+positive_rate INT,
+tests_per_case DECIMAL,
+tests_units TEXT,
+total_vaccinations BIGINT,
+people_vaccinated INT,
+people_fully_vaccinated INT,
+total_boosters BIGINT,
+new_vaccinations BIGINT,
+new_vaccinations_smoothed BIGINT,
+total_vaccinations_per_hundred DECIMAL,
+people_vaccinated_per_hundred DECIMAL,
+people_fully_vaccinated_per_hundred DECIMAL,
+total_boosters_per_hundred DECIMAL,
+new_vaccinations_smoothed_per_million INT,
+new_people_vaccinated_smoothed INT,
+new_people_vaccinated_smoothed_per_hundred INT,
+stringency_index DECIMAL,
+population_density DECIMAL,
+median_age DECIMAL,
+aged_65_older DECIMAL,
+aged_70_older DECIMAL,
+gdp_per_capita DECIMAL,
+extreme_poverty DECIMAL,
+cardiovasc_death_rate DECIMAL,
+diabetes_prevalence DECIMAL,
+female_smokers DECIMAL,
+male_smokers DECIMAL,
+handwashing_facilities DECIMAL,
+hospital_beds_per_thousand DECIMAL,
+life_expectancy DECIMAL,
+human_development_index DECIMAL,
+population BIGINT,
+excess_mortality_cumulative_absolute DECIMAL,
+excess_mortality_cumulative DECIMAL,
+excess_mortality DECIMAL,
+excess_mortality_cumulative_per_million DECIMAL
+);
+
+-- SHOW VARIABLES LIKE "local_infile";
+-- SET GLOBAL local_infile=1;
+
+load data local infile 'C:\\Users\\Dell\\Downloads\\CovidVaccinations.csv'
+INTO TABLE covid_vaccinations 
+fields terminated by ','
+ignore 1 rows;
+
+# SELECT `date`
+# FROM covid_vaccinations ;
+
+
+DROP TABLE IF EXISTS covid_deaths;
+CREATE TABLE covid_deaths (
+iso_code TEXT,
+continent TEXT,
+location TEXT,
+`date` TEXT,
+population bigint,
+total_cases int,
+new_cases bigint,
+new_cases_smoothed DECIMAL,
+total_deaths bigint,
+new_deaths INT,
+new_deaths_smoothed DECIMAL,
+total_cases_per_million DECIMAL,
+new_cases_per_million DECIMAL,
+new_cases_smoothed_per_million DECIMAL,
+total_deaths_per_million DECIMAL,
+new_deaths_per_million DECIMAL,
+new_deaths_smoothed_per_million DECIMAL,
+reproduction_rate DECIMAL,
+icu_patients INT,
+icu_patients_per_million DECIMAL,
+hosp_patients bigint,
+hosp_patients_per_million DECIMAL,
+weekly_icu_admissions BIGINT,
+weekly_icu_admissions_per_million DECIMAL,
+weekly_hosp_admissions bigint,
+weekly_hosp_admissions_per_million DECIMAL
+);
+
+load data local infile 'C:\\Users\\Dell\\Downloads\\CovidDeaths.csv'
+INTO TABLE covid_deaths 
+fields terminated by ','
+ignore 1 rows;
+
+ALTER TABLE covid_deaths
+MODIFY COLUMN `date` date;
+ALTER TABLE covid_vaccinations
+MODIFY COLUMN `date` date;
+
+SELECT * 
+FROM covid_deaths cd
+ORDER BY 3, 4
+;
+
+SELECT * 
+FROM covid_vaccinations cv
+ORDER BY 3, 4;
+
 -- select data that we are using 
 
 Select location, `date`, total_cases, new_cases, total_deaths, population, continent
